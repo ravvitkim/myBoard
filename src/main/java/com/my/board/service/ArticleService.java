446ddd208482bceb96aae4cded2fb259c7efcaac
ravvitkim <mirkim6086@naver.com>
2025-08-29 11:33:04 +0900

    페이징작업완료

--- src/main/java/com/my/board/service/ArticleService.java
+++ src/main/java/com/my/board/service/ArticleService.java
@@ -1,9 +1,12 @@
 package com.my.board.service;
 
+import com.my.board.ArticleRepository;
 import com.my.board.dao.ArticleDao;
 import com.my.board.dto.ArticleDto;
 import com.my.board.entity.Article;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -14,7 +17,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ArticleService {
     private final ArticleDao dao;
-
+    private final ArticleRepository articleRepository;
     public List<ArticleDto> getAllArticle() {
         List<Article> articles = dao.findAllArticle();
         if (ObjectUtils.isEmpty(articles)) {
@@ -23,5 +26,14 @@ public class ArticleService {
             return articles.stream().map(x -> ArticleDto.fromArticle(x))
                     .toList();
         }
+    }
+
+    public Page getArticlePage(Pageable pageable) {
+        Page<Article> articles = articleRepository.findAll(pageable);
+        if (ObjectUtils.isEmpty(articles)) {
+            return null;
+        }
+        return articles.map(x -> ArticleDto.fromArticle(x));
+
     }
 }
